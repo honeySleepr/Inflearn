5da156bfda7431e2b66eaa90c62b5c7ec6cb17e5
honeySleepr <gruzzimo@naver.com>
2022-04-17 18:25:16 +0900

    프론트 컨트롤러 도입 - V1

--- MVC1/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ MVC1/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -10,25 +10,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
 
 @WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
 public class FrontControllerServletV1 extends HttpServlet {
-
+    private static final Logger log = LoggerFactory.getLogger(FrontControllerServletV1.class);
     private Map<String, ControllerV1> controllerMap = new HashMap<>();
 
     public FrontControllerServletV1() {
-        controllerMap.put("/front-controller/v1/members/new-from", new MemberFormControllerV1());
+        // new-from 이라고 오타내서 한참 헤맸네;;
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
         controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
         controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
     }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("FrontController.service");
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws
+        ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        ControllerV1 controller = controllerMap.get(requestURI);
+        log.debug("  ∆˚∆˚∆˚∆˚∆˚∆˚∆˚∆ Controller : {}", controller);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 
+            return;
+        }
+
+        controller.process(request, response);
 
     }
 }
--- MVC1/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ MVC1/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -22,7 +22,7 @@ public class MemberListControllerV1 implements ControllerV1 {
 
         request.setAttribute("members", members);
 
-        String viewPath = "/WEB-INF/views/save-result.jsp";
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
