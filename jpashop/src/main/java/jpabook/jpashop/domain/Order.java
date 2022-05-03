80f245a78e615b72169bbe3c6027f2f3450fa663
honeySleepr <gruzzimo@naver.com>
2022-05-03 12:26:36 +0900

    연관관계 매핑 - 단방향으로 설계하자
    - 단방향 설계로 테이블을 다 만들고 나중에 필요할 경우에만
    양방향으로 바꾸면 된다.
    이 경우 테이블은 바꿀 필요 없이 자바코드만 바꾸면 된다.

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -7,6 +7,8 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,8 +19,10 @@ public class Order {
 	@GeneratedValue
 	@Column(name = "ORDER_ID")
 	private Long id;
-	@Column(name = "MEMBER_ID")
-	private Long memberId;
+
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_ID")
+	private Member member;
 	private LocalDateTime orderDate;
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status;
@@ -31,12 +35,12 @@ public class Order {
 		this.id = id;
 	}
 
-	public Long getMemberId() {
-		return memberId;
+	public Member getMember() {
+		return member;
 	}
 
-	public void setMemberId(Long memberId) {
-		this.memberId = memberId;
+	public void setMember(Member member) {
+		this.member = member;
 	}
 
 	public LocalDateTime getOrderDate() {
