7b08b7da1e3d8a45c618a0d502d99c5daa30e2ba
honeySleepr <gruzzimo@naver.com>
2022-05-06 18:46:21 +0900

    웹 계층 개발 (끝)

--- jpashop2/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop2/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,11 +1,15 @@
 package jpabook.jpashop.controller;
 
+import java.util.List;
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -24,6 +28,46 @@ public class ItemController {
 	public String create(BookForm form) {
 		Book book = new Book();
 		// 실무에서는 setter는 다 날리고 createBook 같은 static 생성메서드를 만들어 사용하는 편이 좋다.
+		book.setPrice(form.getPrice());
+		book.setStockQuantity(form.getStockQuantity());
+		book.setAuthor(form.getAuthor());
+		book.setIsbn(form.getIsbn());
+		book.setName(form.getName());
+
+		itemService.saveItem(book);
+		return "redirect:/";
+	}
+
+	@GetMapping("/items")
+	public String list(Model model) {
+		List<Item> items = itemService.findItems();
+		model.addAttribute("items", items);
+		return "items/itemList";
+	}
+
+	@GetMapping("/items/{itemId}/edit")
+	public String updateItemForm(@PathVariable("itemId") Long intmId, Model model) {
+		Book item = (Book) itemService.findOne(intmId);
+		BookForm form = new BookForm();
+
+		form.setId(item.getId());
+		form.setName(item.getName());
+		form.setPrice(item.getPrice());
+		form.setStockQuantity(item.getStockQuantity());
+		form.setAuthor(item.getAuthor());
+		form.setIsbn(item.getIsbn());
+
+		model.addAttribute("form", form);
+		return "items/updateItemForm";
+	}
+
+	@PostMapping("/items/{itemId}/edit")
+	public String updateItem(@ModelAttribute("form") BookForm form,
+		@PathVariable("itemId") Long itemId) {
+
+		Book book = new Book();
+
+		book.setId(form.getId());
 		book.setName(form.getName());
 		book.setPrice(form.getPrice());
 		book.setStockQuantity(form.getStockQuantity());
@@ -31,6 +75,6 @@ public class ItemController {
 		book.setIsbn(form.getIsbn());
 
 		itemService.saveItem(book);
-		return "redirect:/";
+		return "redirect:/items";
 	}
 }
