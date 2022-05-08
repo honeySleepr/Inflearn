7cfe9ddf653373b4b5ea65e9328bb4f4801030b6
honeySleepr <gruzzimo@naver.com>
2022-05-08 17:17:37 +0900

    API 개발 고급 - 컬렉션 조회 최적화 V6

--- jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop2/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,19 @@
 package jpabook.jpashop.api;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
+
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRespository;
 import lombok.Getter;
@@ -22,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderApiController {
 
 	private final OrderRepository orderRepository;
-	private final OrderQueryRespository orderQueryRespository;
+	private final OrderQueryRespository orderQueryRepository;
 
 	@GetMapping("/api/v1/orders")
 	public List<Order> ordersV1() {
@@ -42,8 +47,8 @@ public class OrderApiController {
 	public List<OrderDto> ordersV2() {
 		List<Order> orders = orderRepository.findAllByString(new OrderSearch());
 		return orders.stream() // #1 Order 쿼리
-					 .map(OrderDto::new) // #2~#5 쿼리
-					 .collect(Collectors.toList());
+			.map(OrderDto::new) // #2~#5 쿼리
+			.collect(toList());
 	}
 
 	/* 쿼리 한방으로 해결되지만, 일대다 join fetch 한 경우 페이징이 메모리에서 이뤄지기 때문에 페이징을 쓰면 안된다!*/
@@ -52,8 +57,8 @@ public class OrderApiController {
 	public List<OrderDto> ordersV3() {
 		List<Order> orders = orderRepository.findAllWithItem();
 		return orders.stream()
-					 .map(OrderDto::new)
-					 .collect(Collectors.toList());
+			.map(OrderDto::new)
+			.collect(toList());
 	}
 
 	/* 테이블을 정규화된 형식으로(중복값 없이) 당겨온다. v3에 비해 쿼리는 더 많지만 중복이 없기 때문에 페이징이 가능하다*/
@@ -65,18 +70,34 @@ public class OrderApiController {
 		// `default_batch_fetch_size` 옵션을 통해 List<Order> 와 관련된 데이터를 `in` 쿼리를 통해 미리 가져온다
 		List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
 		return orders.stream()
-					 .map(OrderDto::new)
-					 .collect(Collectors.toList());
+			.map(OrderDto::new)
+			.collect(toList());
 	}
 
 	@GetMapping("/api/v4/orders")
 	public List<OrderQueryDto> ordersV4() {
-		return orderQueryRespository.findOrderQueryDtos();
+		return orderQueryRepository.findOrderQueryDtos();
 	}
 
 	@GetMapping("/api/v5/orders")
 	public List<OrderQueryDto> ordersV5() {
-		return orderQueryRespository.findAllByDto_optimization();
+		return orderQueryRepository.findAllByDto_optimization();
+	}
+
+	// 그냥 V5 쓰자
+	@GetMapping("/api/v6/orders")
+	public List<OrderQueryDto> ordersV6() {
+		List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+		return flats.stream()
+			.collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),
+					o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+				mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+					o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+			)).entrySet().stream()
+			.map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+				e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+				e.getKey().getAddress(), e.getValue()))
+			.collect(toList());
 	}
 
 	@Getter
@@ -98,10 +119,10 @@ public class OrderApiController {
 			this.orderStatus = order.getStatus();
 			this.address = order.getDelivery().getAddress(); // #3 Delivery 쿼리
 			order.getOrderItems().stream()
-				 .forEach(o -> o.getItem().getName()); // #4, #5 OrderItems 쿼리, Item 쿼리 까지!!
+				.forEach(o -> o.getItem().getName()); // #4, #5 OrderItems 쿼리, Item 쿼리 까지!!
 			this.orderItems = order.getOrderItems().stream()
-								   .map(OrderItemDto::new)
-								   .collect(Collectors.toList());
+				.map(OrderItemDto::new)
+				.collect(toList());
 		}
 	}
 
