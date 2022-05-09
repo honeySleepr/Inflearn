b1907584ea5592b8704cb9821015ff8591390ebb
honeySleepr <gruzzimo@naver.com>
2022-05-10 01:36:47 +0900

    :sparkles: 프로젝트 환경 설정, 예제 도메인 모델 생성

---
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,53 @@
+package study.datajpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+	@Id @GeneratedValue
+	@Column(name = "member_id")
+	private Long id;
+	private String username;
+	private int age;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_id")
+	private Team team;
+
+	// JPA에서 @Entity 객체에 Proxy를 사용하기 위해 기본 생성자가 필요하다
+	// `@NoArgsConstructor(access = AccessLevel.PROTECTED)` 로 대신한다!
+//	protected Member() {
+//	}
+
+	public Member(String username) {
+		this.username = username;
+	}
+
+	public Member(String username, int age, Team team) {
+		this.username = username;
+		this.age = age;
+		if (team != null) {
+			changeTeam(team);
+		}
+	}
+
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
+	}
+}
